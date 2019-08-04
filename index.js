42b32d7ea9015d9b13bdb6654c7caec133db58ac
werachon phunta <werachonp@gmail.com>
2019-08-04 07:30:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -11,4 +11,22 @@ server()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({extended: false}))
     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
+    // เพิ่มส่วนของ Webhook เข้าไป
+    .post('/webhook', function (req, res) {
+        let replyToken = req.body.events[0].replyToken;
+        let message = req.body.events[0].message.text;
+        
+        console.log(`Message token : ${ replyToken }`);
+        console.log(`Message from chat : ${ message }`);
+
+        lineMessaging.replyMessage(replyToken, message).then(function (rs) {
+
+            console.log(`Reply message result : ${ rs }`);
+
+            res.json({
+                status: 200,
+                message: `Sent message!`
+            });
+        });
+    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
